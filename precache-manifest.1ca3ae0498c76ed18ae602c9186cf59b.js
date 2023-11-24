cc3c85a51e85419e06540cc75e24037dfa3008ae
arjun777is <arjun777is@gmail.com>
2023-11-24 13:11:34 +0530

    Updates

---
+++ precache-manifest.1ca3ae0498c76ed18ae602c9186cf59b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83a4a44b6840ce6d50a3c63da29fef5e",
+    "url": "/ToDoApp-React/index.html"
+  },
+  {
+    "revision": "aa0fcafb9950eeae4ca0",
+    "url": "/ToDoApp-React/static/css/main.4d2d48e7.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/ToDoApp-React/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "aa0fcafb9950eeae4ca0",
+    "url": "/ToDoApp-React/static/js/main.bc897851.chunk.js"
+  },
+  {
+    "revision": "b57993952a4ca3ee8a9d",
+    "url": "/ToDoApp-React/static/js/runtime~main.df81f5ae.js"
+  }
+]);
